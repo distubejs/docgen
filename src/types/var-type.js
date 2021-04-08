f574093a048dfe2ee76b5ba380342f63010e2ab9
Skick <skick1234@gmail.com>
2021-04-08 10:14:51 +0700

    Keep dot when splitting type name

--- src/types/var-type.js
+++ src/types/var-type.js
@@ -18,13 +18,14 @@ class DocumentedVarType extends DocumentedItem {
 
 	static splitVarName(str) {
 		if(str === '*') return ['*'];
-		str = str.replace(/\./g, '');
-		const matches = str.match(/([\w*]+)([^\w*]+)/g);
+		str = str.replace(/\.</g, '<');
+		const matches = str.match(/([\w*]+)([^\w*]?)/g);
 		const output = [];
 		if(matches) {
 			for(const match of matches) {
 				const groups = match.match(/([\w*]+)([^\w*]+)/);
-				output.push([groups[1], groups[2]]);
+				if(!groups) output.push([match]);
+				else output.push([groups[1], groups[2]]);
 			}
 		} else {
 			output.push([str.match(/([\w*]+)/g)[0]]);
